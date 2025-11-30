08a394dc440f20868fd7f6d821581945aba8a8fd
DanielKyjas <dani.kyjas@hotmail.com>
2025-11-30 18:36:12 +0100

    WorkingNavBar

--- app/src/main/java/com/example/marvelallies/MainActivity.kt
+++ app/src/main/java/com/example/marvelallies/MainActivity.kt
@@ -3,18 +3,43 @@ package com.example.marvelallies
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.example.marvelallies.databinding.ActivityMainBinding
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(News())
+
+        binding.bottomNavigationView.setOnItemSelectedListener{
+            when(it.itemId){
+                R.id.news -> replaceFragment(News())
+                R.id.characters -> replaceFragment(Characters())
+                R.id.players -> replaceFragment(Players())
+                R.id.profile -> replaceFragment(Profile())
+
+                else -> {
+
+                }
+            }
+            true
         }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout,fragment)
+        fragmentTransaction.commit()
     }
 }
