29e544b7557b9973630ebe2d6713c8dc34334485
Juan_Pablor_SR <133048152+JuanPabloSaavedraRamirez@users.noreply.github.com>
2025-12-12 17:31:23 +0100

    fix bug api

--- app/src/main/java/MarvelAPI/MarvelResponse.kt
+++ app/src/main/java/MarvelAPI/MarvelResponse.kt
@@ -1,19 +1,24 @@
 //Aqui podremos agrego toda las data class que podemos ir necesitando, para player stats, heroes stats, etc.
 
 //Hero Data
-data class MarvelCharacter(
-    val id: Int,
-    val image: String,
+
+data class Hero(
+    val id: String,
     val name: String,
-    val alias: String,
+    val real_name: String?,
+    val imageUrl: String,
     val role: String,
+    val attack_type: String?,
+    val team: List<String>?,
+    val difficulty: String?,
+    val bio: String?,
     val abilities: List<Ability>
 )
 
 data class Ability(
     val ability_name: String,
-    val cooldown: Int,
     val description: String
 )
+
 
 //Player Stats
