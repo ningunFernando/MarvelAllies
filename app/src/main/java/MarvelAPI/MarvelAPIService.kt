52f9834ce31f0973c7f081875b953b192ea5f328
Juan_Pablor_SR <133048152+JuanPabloSaavedraRamirez@users.noreply.github.com>
2025-12-16 14:45:54 +0100

    add fun
    functions:
    - GetHeroByID
    - GetPlayerByUsername

--- app/src/main/java/MarvelAPI/MarvelAPIService.kt
+++ app/src/main/java/MarvelAPI/MarvelAPIService.kt
@@ -4,12 +4,22 @@ import Hero
 import Player
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface MarvelAPIService
 {
+    //Heroes
     @GET("v1/heroes")
     fun getAllHeroes(): Call<List<Hero>>
 
+    //LLamar a un heroe en especifico
+    @GET("heroes/hero/{query}")
+    fun getHeroById(@Path("query") query: String):Call<Hero>
+
+    //Jugadores
     @GET("v2/player")
     fun gelAllPlayers(): Call<List<Player>>
+
+    @GET("v1/find-player/{username}")
+    fun getPlayerByName(@Path("username") username: String):Call<Player>
 }
