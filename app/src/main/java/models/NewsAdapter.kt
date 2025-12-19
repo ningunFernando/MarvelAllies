63be79aceee81eb85a34ae4decdbca9d8bd7f238
DanielKyjas <dani.kyjas@hotmail.com>
2025-12-19 13:23:40 +0100

    NewsAdapter

---
+++ app/src/main/java/models/NewsAdapter.kt
@@ -0,0 +1,63 @@
+package models
+
+import com.example.marvelallies.R
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+
+class NewsAdapter(private val news: List<NewsItem>,
+                  private val onItemClickListener: (NewsItem) -> Unit
+): RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): NewsAdapter.NewsViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.news_item, parent, false)
+
+        return NewsViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: NewsAdapter.NewsViewHolder, position: Int) {
+        val player = news[position]
+        holder.bind(player, onItemClickListener)
+    }
+
+    override fun getItemCount(): Int {
+        return news.size
+    }
+
+    class NewsViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+            private val newsTitle: TextView = itemView.findViewById(R.id.NewsTitle)
+            private val newsImage: ImageView = itemView.findViewById(R.id.NewsImage)
+            private val newsLayout: LinearLayout = itemView.findViewById(R.id.NewsLayout)
+
+        fun bind(new: NewsItem, listener: (NewsItem) -> Unit){
+
+            newsTitle.text = new.title
+            Glide.with(itemView.context)
+                //Si carga toma la imagen de este URL
+                .load("https://marvelrivalsapi.com/rivals"+new.imageUrl)
+                //Si no pone una de placeholder
+                .placeholder(R.drawable.frame_1)
+                //Acomoda la imagen en el centro del item
+                .fitCenter()
+                //En la imagen del item
+                .into(newsImage)
+
+            newsLayout.setOnClickListener {
+                // Llama al callback
+                listener(new)
+            }
+
+        }
+
+
+    }
+
+}
