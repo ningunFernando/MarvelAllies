3db724f39dabf7f0be97707b24c0e311eb4b5bbd
ningunFernando <117299803+ningunFernando@users.noreply.github.com>
2025-12-19 14:36:33 +0100

    Revert "SDK UPDATE"
    This reverts commit 4cd11382e994e9dbc09cffb07dbaac0035e8e40b.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 android {
     namespace = "com.example.marvelallies"
-    compileSdk = 36
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.marvelallies"
@@ -33,11 +33,11 @@ android {
 
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "1.8"
     }
 }
 
