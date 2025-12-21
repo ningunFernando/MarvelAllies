8b1c7b72cd410c9986416598ece7895a0d78187b
ningunFernando <117299803+ningunFernando@users.noreply.github.com>
2025-12-21 16:06:06 +0100

    Google Login Created

--- app/src/main/java/com/example/marvelallies/LoginActivity.kt
+++ app/src/main/java/com/example/marvelallies/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.example.marvelallies
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -8,6 +10,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
@@ -16,18 +23,34 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var passwordField: EditText
     private lateinit var auth: FirebaseAuth
 
+    private lateinit var googleSingInClient: GoogleSignInClient
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        emailField = findViewById(R.id.login_etEmail)
-        passwordField = findViewById(R.id.login_etPassword)
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("341760355003-kqopo7bfp4aqqbg09s5ecmjjk0it3bti.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+
+        googleSingInClient = GoogleSignIn.getClient(this, gso)
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+
+        account?.let{
+            Log.d("Google Login", "${it.displayName} Account is already logged in")
+        } ?: run{
+            Log.d("Google Login", "No account is logged in")
+            findViewById<SignInButton>(R.id.login_btnGoogle).setOnClickListener {SingInWithGoogle()}
+        }
 
         auth = FirebaseAuth.getInstance()
 
+        emailField = findViewById(R.id.login_etEmail)
+        passwordField = findViewById(R.id.login_etPassword)
+
         findViewById<Button>(R.id.login_btnRegister).setOnClickListener {Register()}
         findViewById<Button>(R.id.login_btnLogin).setOnClickListener {Login()}
-
     }
 
     private fun Register(){
@@ -68,4 +91,26 @@ class LoginActivity : AppCompatActivity() {
         auth.signOut()
         Toast.makeText(this, "Logout successful", Toast.LENGTH_SHORT).show()
     }
+
+    private fun SingInWithGoogle() {
+        val singInIntent = googleSingInClient.signInIntent
+        startActivityForResult(singInIntent, 9001)
+        Toast.makeText(this, "Login with Google", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 9001){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            if(task.isSuccessful){
+                val account = task.getResult(ApiException::class.java)
+                Log.d("Google Login", "Account name: ${account?.displayName}")
+            }else{
+                Log.w("Google Login", "Google sign in failed", task.exception)
+            }
+        }
+
+    }
+
 }
